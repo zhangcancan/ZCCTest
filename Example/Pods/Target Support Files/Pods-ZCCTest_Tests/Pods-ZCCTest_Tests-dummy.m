b09346f7a3eb857fc47626db6e0a0bb9c4e9ec8e
Zhangcancan <zhangcancan@gomeplus.com>
2018-04-20 17:52:56 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-ZCCTest_Tests/Pods-ZCCTest_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZCCTest_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZCCTest_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZCCTest/ZCCTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZCCTest : NSObject
+@end
+@implementation PodsDummy_ZCCTest
+@end
